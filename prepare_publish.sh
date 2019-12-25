9174ffaf6e0f55a3820f45b64c75619cc4cc7bd6
Anton Bessonov <anton.bessonov.public@gmail.com>
2019-12-25 21:04:51 +0100

    fix publisher

--- prepare_publish.sh
+++ prepare_publish.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 PACKAGE_VERSION=$(node -p "require('./package.json').version")
-git tag "v$PACKAGE_VERSION"
 
 npm version patch
 
-git commit -m "Release $PACKAGE_VERSION"
+git add package.json
+
+git commit -m "Release v$PACKAGE_VERSION"
+git tag "v$PACKAGE_VERSION"
