2e28ab1a5611941e0074d355413c51ab43203e8d
Anton Bessonov <anton.bessonov.public@gmail.com>
2021-05-15 13:48:23 +0200

    update rules

--- typescript.js
+++ typescript.js
@@ -1,17 +1,29 @@
 module.exports = {
 	rules: {
+		// https://stackoverflow.com/questions/63818415/react-was-used-before-it-was-defined/64024916#64024916
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-use-before-define': 'error',
 		// seems to be broken with typescript
 		'import/no-unresolved': 'off',
 		// raise from warning
 		'@typescript-eslint/no-unused-vars': 'error',
-		// even ts-ignore is discouraged, it's enough to have it as a red flag
-		'@typescript-eslint/ban-ts-ignore': 'off',
-		'@typescript-eslint/explicit-function-return-type': 'error',
+		'@typescript-eslint/ban-ts-comment': [
+			'off',
+			{
+				'ts-expect-error': 'allow-with-description',
+				'ts-ignore': 'allow-with-description',
+				'ts-nocheck': 'allow-with-description',
+				'ts-check': 'allow-with-description',
+				minimumDescriptionLength: 3,
+			},
+		],
 		'@typescript-eslint/explicit-member-accessibility': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'error',
 		'@typescript-eslint/no-angle-bracket-type-assertion': 'off',
 		'@typescript-eslint/no-object-literal-type-assertion': 'off',
 		'@typescript-eslint/no-explicit-any': 'error',
 		'@typescript-eslint/no-extra-parens': 'error',
+		'@typescript-eslint/no-floating-promises': 'error',
 		// no semicolons
 		'@typescript-eslint/member-delimiter-style': [
 			'error',
